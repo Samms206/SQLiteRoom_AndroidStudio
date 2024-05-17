33d5da6dbf36057c94d51a8f245c2b554b4787f9
Samms206 <sh2069936@gmail.com>
2024-05-18 00:22:47 +0700

    updated

--- app/src/main/java/com/example/modul4/AddPostActivity.kt
+++ app/src/main/java/com/example/modul4/AddPostActivity.kt
@@ -1,14 +1,160 @@
 package com.example.modul4
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.esafirm.imagepicker.features.ImagePickerConfig
+import com.esafirm.imagepicker.features.ImagePickerMode
+import com.esafirm.imagepicker.features.ReturnMode
+import com.esafirm.imagepicker.features.registerImagePicker
+import com.example.modul4.room.PostDatabase
+import com.example.modul4.room.PostViewModel
+import com.example.modul4.room.PostViewModelFactory
+import com.example.modul4.utils.reduceFileImage
+import com.example.modul4.utils.uriToFile
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 
 class AddPostActivity : AppCompatActivity() {
+
+    // Mendeklarasikan variabel untuk menyimpan URI gambar yang dipilih
+    private var currentImageUri: Uri? = null
+    // Mendeklarasikan ImageView untuk menampilkan gambar yang dipilih
+    private lateinit var playerImage: ImageView
+    // Mendeklarasikan ViewModel untuk interaksi dengan database
+    private lateinit var postViewModel: PostViewModel
+    //
+    private lateinit var vPostDesc: TextInputEditText
+    // Mendeklarasikan EditText
+    private lateinit var vPostImage: ImageView
+
+    private lateinit var vText_img: TextView
+
+
+    // Mendeklarasikan image picker untuk memilih gambar dari galeri
+    private val imagePickerLauncher = registerImagePicker {
+        val firstImage = it.firstOrNull() ?: return@registerImagePicker
+        if (firstImage.uri.toString().isNotEmpty()) {
+            // Menampilkan ImageView jika gambar berhasil dipilih
+            playerImage.visibility = View.VISIBLE
+            // Menyimpan URI gambar yang dipilih
+            currentImageUri = firstImage.uri
+            // Menampilkan pesan bahwa gambar berhasil dimasukkan
+            vText_img.setText("change")
+
+            // Menggunakan library Glide untuk menampilkan gambar yang dipilih
+            Glide.with(playerImage)
+                .load(firstImage.uri)
+                .into(playerImage)
+        } else {
+            // Menyembunyikan ImageView jika tidak ada gambar yang dipilih
+            View.GONE
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_post)
+
+        // Mendapatkan instance ViewModel
+        val factory = PostViewModelFactory.getInstance(this) //ini
+        postViewModel = ViewModelProvider(this, factory)[PostViewModel::class.java] //ini
+
+        // Menghubungkan variabel dengan komponen di layout
+        vPostImage = findViewById(R.id.post_img_edit)
+        vPostDesc = findViewById(R.id.post_desc_edit)
+        vText_img = findViewById(R.id.text_img)
+
+        // Memanggil fungsi onClick untuk menangani aksi klik
+        onClick()
+    }
+
+    private fun onClick() {
+        // Menangani aksi klik pada EditText untuk memilih gambar
+        val openImagePicker = findViewById<ImageView>(R.id.post_img_edit)
+        openImagePicker.setOnClickListener {
+            imagePickerLauncher.launch(
+                ImagePickerConfig {
+                    mode = ImagePickerMode.SINGLE
+                    returnMode = ReturnMode.ALL
+                    isFolderMode = true
+                    folderTitle = "Galeri"
+                    isShowCamera = false
+                    imageTitle = "Click to choice the image"
+                    doneButtonText = "Done"
+                }
+            )
+        }
+
+        // Menangani aksi klik pada tombol simpan
+        val btnSavedPlayer = findViewById<Button>(R.id.btn_savedPost)
+        btnSavedPlayer.setOnClickListener {
+            // Memvalidasi input dan menyimpan data jika valid
+            if (validateInput()) {
+                savedData()
+            }
+        }
+    }
+
+    // Fungsi untuk memvalidasi input
+    private fun validateInput(): Boolean {
+        var error = 0
+
+        // Memeriksa apakah nama pemain kosong
+        if (vPostDesc.text.toString().isEmpty()) {
+            error++
+            vPostDesc.error = "Desc is not empty!"
+        }
+
+        // Memeriksa apakah gambar pemain kosong
+        if (vText_img.text.toString() == "add") {
+            error++
+            vText_img.error = "Image is not Empty!"
+        }
+
+        // Mengembalikan true jika tidak ada error, false jika ada error
+        return error == 0
+    }
+
+    // Fungsi untuk menyimpan data pemain
+    private fun savedData() {
+        // Mengubah URI gambar menjadi file dan mengurangi ukuran file
+        val imageFile = currentImageUri?.let { uriToFile(it, this).reduceFileImage() }
+
+        // Membuat objek pemain dengan data yang diinputkan
+        val post = imageFile?.let {
+            val descriptionText = vPostDesc.text.toString()
+            val words = descriptionText.split(" ")
+            val firstTwoWords = words.take(2).joinToString(" ")
+            PostDatabase(
+                id = 0,
+                name = firstTwoWords,
+                description = descriptionText,
+                image = imageFile,
+                like = 0
+            )
+        }
+
+        // Menyimpan data pemain ke database
+        if (post != null) postViewModel.insertPost(post)
+
+        // Menampilkan pesan bahwa data pemain berhasil ditambahkan
+        Toast.makeText(
+            this@AddPostActivity,
+            "Data Success Added",
+            Toast.LENGTH_SHORT
+        ).show()
+
+        // Mengakhiri activity
+        finish()
     }
 
     fun toMain(view: View) {
