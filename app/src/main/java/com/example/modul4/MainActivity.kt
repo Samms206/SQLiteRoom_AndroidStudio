7aa8ab65cee2de5d0c5f07ca743c835965613445
Samms206 <sh2069936@gmail.com>
2024-05-17 20:55:40 +0700

    updated

--- app/src/main/java/com/example/modul4/MainActivity.kt
+++ app/src/main/java/com/example/modul4/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.modul4
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    }
+
+    fun toAddPost(view: View) {
+        val intent = Intent(this, AddPostActivity::class.java)
+        startActivity(intent)
     }
 }
